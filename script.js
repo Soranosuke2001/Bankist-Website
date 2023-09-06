5adda587b914dcf5f84613e0fc26baa5be76668a
Sora <80365991+Soranosuke2001@users.noreply.github.com>
2023-09-06 10:17:16 -0700

    added comments and refactored code

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 "use strict";
 
-///////////////////////////////////////
 // Modal window
+// --------------------------------------------------------------------------
 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+// Function to open the modal
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
+// Function to hide the modal
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
 
+// Open modal button
 btnsOpenModal.forEach(btn => btn.addEventListener("click", openModal));
 
+// Close modal on button click, overlay click or ESC key
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
@@ -30,12 +34,16 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-// Displaying a cookie message
+// Cookie Message
+// --------------------------------------------------------------------------
+
+const header = document.querySelector(".header");
 const message = document.createElement("div");
+
 message.classList.add("cookie-message");
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-headerElClass.append(message);
+header.append(message);
 
 // Removing the cookie message on button click
 const closeCookieBtn = document.querySelector(".btn--close-cookie");
@@ -45,7 +53,8 @@ closeCookieBtn.addEventListener("click", () => {
 
 // Styles for the cookie message
 message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
+message.style.width = "100%";
 
 // Adding more height to the cookie message
-message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
